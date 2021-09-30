fc051e4b1aa895b3bbd30e33b3235e2d43257e7b
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-09-30 18:15:09 -0400

    Exercise 25 skeleton

--- Exercise24/src/main/java/baseline/Solution24.java
+++ Exercise24/src/main/java/baseline/Solution24.java
@@ -11,20 +11,69 @@
  */
 package baseline;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Solution24 {
     public static void main(String[] args) {
-
+        input();
     }
 
     //This function creates a scanner and takes in input
     private static void input()
     {
+        Scanner in = new Scanner(System.in);
+        String a,b;
+
         //Prompt for 2 strings
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
+
+        System.out.println("Enter the first string:");
+        a = in.nextLine();
+        System.out.println("Enter the second string:");
+        b = in.nextLine();
+
+        //Pass through isAnagram
+
+        if(isAnagram(a,b))
+        {
+            System.out.println("\""+a+"\" and \""+b+"\" are anagrams.");
+        }
+        else
+        {
+            System.out.println("\""+a+"\" and \""+b+"\" are not anagrams.");
+        }
+
+
+
     }
+
     //This function compares 2 strings to check if they are anagrams
     private static boolean isAnagram(String a, String b)
     {
         //First we will reverse string a and check if string a is
         //equal to string b
+
+        char[] tempA = a.toCharArray();
+        char[] tempB = b.toCharArray();
+
+        if(a.length() != b.length())
+        {
+            return false;
+        }
+
+        Arrays.sort(tempA);
+        Arrays.sort(tempB);
+
+        for(int i = 0; i < a.length(); i++)
+        {
+            if (tempA[i] != tempB[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
---
+++ Exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Take in input using an input() function
+        -This function is called from main
+        -This function will prompt for and
+        receive a string
+    -Then inside of input pass the string into a function called passwordValidator
+        -This will determine whether a string is
+            -Very weak
+            -Weak
+            -Strong
+            -Very Strong
+        -Then it will return an integer
+    -This integer will be used inside of input() to determine the output
+ */
+package baseline;
+
+public class Solution25 {
+    //Calls input
+    public static void main(String[] args) {
+
+    }
+
+    //Takes in input and displays output
+    private static void input()
+    {
+
+    }
+
+    //Takes in a string and checks how strong it is
+    private static int passwordValidator(String password)
+    {
+
+    }
+
+}
