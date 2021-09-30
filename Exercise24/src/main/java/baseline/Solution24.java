23e7115cd0b3a73d6903b61ec3c1ec4bf6494bc6
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-09-30 16:58:55 -0400

    ACTUALLY added exercise24 skeleton

--- Exercise24/src/main/java/baseline/Solution24.java
+++ Exercise24/src/main/java/baseline/Solution24.java
@@ -1,2 +1,30 @@
-package baseline;public class Solution24 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Prompt and take in user input in an input function.
+        -2 input strings are needed
+    -Pass those 2 input strings through a function called isAnagram
+    -Display an output based on whether isAnagram returns true
+ */
+package baseline;
+
+public class Solution24 {
+    public static void main(String[] args) {
+
+    }
+
+    //This function creates a scanner and takes in input
+    private static void input()
+    {
+        //Prompt for 2 strings
+    }
+    //This function compares 2 strings to check if they are anagrams
+    private static boolean isAnagram(String a, String b)
+    {
+        //First we will reverse string a and check if string a is
+        //equal to string b
+    }
 }
