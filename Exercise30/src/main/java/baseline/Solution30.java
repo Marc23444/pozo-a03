d083e762750cda0e4eadeda4fb51ee1bfb65d31f
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-09-30 18:24:57 -0400

    Problem 30 Skeleton

---
+++ Exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create a function called output that will print out a multiplication
+    table that displays 1-12 inclusive
+
+ */
+package baseline;
+
+public class Solution30 {
+
+    //Calls output
+    public static void main(String[] args) {
+
+    }
+
+    //Will display the table evenly spaced
+    private static void output()
+    {
+        //This function will use a nested for loop
+
+
+    }
+}
