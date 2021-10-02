5f30616096b97ed82ab461280cf222133558a1b0
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 16:31:03 -0400

    Finished 28, no junit yet

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -11,16 +11,30 @@
  */
 package baseline;
 
+//Scanner will be used to take in input
+import java.util.Scanner;
+
 public class Solution28 {
     //Driver function, will call input
     public static void main(String[] args) {
-
+        input();
     }
 
     //This function will take in 5 inputs using a scanner
     private static void input()
     {
         //Creates a scanner, uses a for loop to prompt for input.
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+
+        //For loop from 1 to 5 to grab 5 inputs
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println("Enter a number: ");
+            sum += in.nextInt();
+        }
+
+        System.out.println("The total is "+sum+".");
 
     }
 }
