497d51d281c3a0331dfc2006081d01e98c85ccc6
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 15:34:55 -0400

    Finished 26, no unit testing

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -1,9 +1,17 @@
 package baseline;
 
 public class PaymentCalculator {
-    //This class will calculate the months using no parameters
-    public static int calculateMonthsUntilPaidOff()
+    //This class will calculate the months
+    public double calculateMonthsUntilPaidOff(double i, double b, double p)
     {
+        //Once months is created it is set through 3 separate calculations
+        double months = (-1.0/30.0);
 
+        months = months * Math.log((1+(b/p) * (1 - Math.pow((1 + i),30))));
+
+        months = months / Math.log((1 + i));
+
+        //Rounds up
+        return Math.ceil(months);
     }
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -14,16 +14,37 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution26 {
     //Where input will be called
     public static void main(String[] args) {
-
+        input();
     }
 
     //We will take in input then calculate and display the output within this function
     private static void input()
     {
         //This function will utilize the function calculateMonthsUntilPaidOff from the PaymentCalculator class
+        Scanner in = new Scanner(System.in);
+        double balance, APR, monthlyPayment, totMonths;
+
+        System.out.println("What is your balance?");
+        balance = in.nextDouble();
+
+        System.out.println("What is the APR on the card (as a percent)?");
+        APR = in.nextDouble();
+        APR = APR / 100;
+
+        System.out.println("What is the monthly payment you can make?");
+        monthlyPayment = in.nextDouble();
+
+        PaymentCalculator calculator = new PaymentCalculator();
+
+        totMonths = calculator.calculateMonthsUntilPaidOff((APR / 365),balance,monthlyPayment);
+
+        System.out.println("It will take you "+ ((int) totMonths) +" months to pay off this card.\n");
+
 
     }
 }
