2e8d0f18353a0eaeedcd9478f93b1402caac7b19
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-01 23:47:42 -0400

    Exercise 26 skeleton

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,9 @@
+package baseline;
+
+public class PaymentCalculator {
+    //This class will calculate the months using no parameters
+    public static int calculateMonthsUntilPaidOff()
+    {
+
+    }
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input function to take in input
+        -Ask for the balance
+        -Ask for APR as a percent
+        -Ask for monthly payment rate
+    -After all input is grabbed use the calculateMonthsUntilPaidOff function
+    in the PaymentCalculator class to determine the output
+    -Display the output in the input method
+ */
+package baseline;
+
+public class Solution26 {
+    //Where input will be called
+    public static void main(String[] args) {
+
+    }
+
+    //We will take in input then calculate and display the output within this function
+    private static void input()
+    {
+        //This function will utilize the function calculateMonthsUntilPaidOff from the PaymentCalculator class
+
+    }
+}
