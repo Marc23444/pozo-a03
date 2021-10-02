aab614795e1ac7482b665b8993ac018a0f3551fe
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 16:27:24 -0400

    Exercise 28 skeleton created

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input function to handle all user input
+        -This function will have a for loop that takes in input 5 times
+        -The input will be in integers
+    -This input will then be added and displayed
+ */
+package baseline;
+
+public class Solution28 {
+    //Driver function, will call input
+    public static void main(String[] args) {
+
+    }
+
+    //This function will take in 5 inputs using a scanner
+    private static void input()
+    {
+        //Creates a scanner, uses a for loop to prompt for input.
+
+    }
+}
