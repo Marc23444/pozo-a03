148b942808be66885302808413b1828bd21cd179
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 18:01:49 -0400

    Added exercise 32 skeleton as well as all other folders

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -The program starts with a function called difficultyInput
+    -Once this function has gotten valid input, it will pass the difficulty level chosen to the function gameLoop
+    -Gameloop
+        -Will generate a random number based on the difficulty level
+        -Will have a while loop that prompts the user for input
+        -Will check if the user is right or wrong, will keep track of total guesses
+            -The program will provide feedback based on a users preformance
+                -If the number guessed was too high the user is told
+                -If  the number guessed was too low the user is also told
+            -Once the correct number is found the loop will exit
+        -Once out of the loop the program will ask the user to play again, this will result in a call to difficultyInput
+        and the cycle will continue
+
+ */
+package baseline;
+
+public class Solution32 {
+
+    //Driver function, runs difficultyInput
+    public static void main(String[] args) {
+
+    }
+
+    //Takes in and validates a difficulty setting from the user
+    private static void difficultyInput()
+    {
+        //Uses a while with a try catch inside
+
+    }
+
+    //This function contains the bulk of the function and is the brains behind the game
+    private static void gameLoop(int difficulty)
+    {
+        //Uses a while loop
+
+    }
+
+}
