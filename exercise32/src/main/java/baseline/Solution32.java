099624cef56451db329a5fca608af2225fd34615
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 18:22:20 -0400

    Exercise 32 Finished, no junit

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -20,24 +20,129 @@
  */
 package baseline;
 
+import java.util.Random;
+import java.util.RandomAccess;
+import java.util.Scanner;
+
 public class Solution32 {
 
     //Driver function, runs difficultyInput
     public static void main(String[] args) {
-
+        difficultyInput();
     }
 
     //Takes in and validates a difficulty setting from the user
     private static void difficultyInput()
     {
         //Uses a while with a try catch inside
+        Scanner in = new Scanner(System.in);
+        boolean validInput = false;
+        int difficulty = 0;
+
+
+        System.out.println("Let's play Guess the Number!");
+
+        while (!validInput)
+        {
+
+            try{
+                System.out.println("Enter the difficulty level (1, 2, or 3):");
+                difficulty = Integer.parseInt(in.nextLine());
+
+                if(difficulty == 1 ||difficulty == 2 ||difficulty == 3)
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input.");
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Invalid input.");
+            }
+        }
+
+        gameLoop(difficulty);
+
 
     }
 
     //This function contains the bulk of the function and is the brains behind the game
     private static void gameLoop(int difficulty)
     {
-        //Uses a while loop
+        //Uses a while loop and a random number generator
+
+        int secretNum, guess, guessCount = 1;
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        String playAgain;
+
+        //This switch determines the max number the secret number can be.
+        switch (difficulty)
+        {
+            case 1:
+                secretNum = rand.nextInt(10);
+                break;
+            case 2:
+                secretNum = rand.nextInt(100);
+                break;
+            case 3:
+                secretNum = rand.nextInt(1000);
+                break;
+            default:
+                secretNum = 0;
+                break;
+        }
+        //GAME STARTS HERE
+        //These try catch statements make sure the user enters valid input.
+        System.out.println("I have my number. What's your guess?");
+        try {
+            guess = Integer.parseInt(in.nextLine());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Invalid input, this will be counted as a guess.");
+            guess = 0;
+        }
+
+
+        //While loop that runs while the guess is wrong
+        //Try catches are used to make sure the input is valid
+        while (guess != secretNum)
+        {
+            //This if else blocks provides the user with feedback
+            if(guess < secretNum)
+            {
+                System.out.println("Too low. Guess again:");
+            }
+            else if (guess > secretNum)
+            {
+                System.out.println("Too high. Guess again:");
+            }
+
+            try {
+                guess = Integer.parseInt(in.nextLine());
+            }
+            catch (Exception e)
+            {
+                System.out.println("Invalid input, this will be counted as a guess.");
+            }
+
+            guessCount++;
+
+        }
+
+        //Finally the program tells the user the amount of guesses it took and prompts them to play again.
+        System.out.println("You got it in "+guessCount+" guesses!\n");
+        System.out.println("Do you wish to play again (Y/N)?");
+        playAgain = in.nextLine();
+
+        if(playAgain.equals("y")||playAgain.equals("Y"))
+        {
+            difficultyInput();
+        }
 
     }
 
