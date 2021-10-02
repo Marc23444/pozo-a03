5513b54d6f4e2c62867d25b72588d845e78b35f2
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 16:38:59 -0400

    Exercise 29 skeleton completed

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -The program will have a function called input that will take in user input
+        -Input will take the rate of return
+    -This input function will then check:
+        -That r is > 0
+        -That r is a number
+    -If either of those conditions are not met the program will prompt for input again
+    until the conditions are met.
+    -Input will send r to a seperate function called calcYears which will then return
+    the years. years will then be output in input
+ */
+package baseline;
+
+public class Solution29 {
+    //Driver function, runs input
+    public static void main(String[] args) {
+
+    }
+
+    //Input function, takes in r and makes sure it meets the conditions
+    private static void input()
+    {
+
+    }
+
+    //Will return years = 72 / r
+    private static double calcYears(double r)
+    {
+
+    }
+}
