f0c99d43886b983933ba3d5f37603802960b81f0
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 18:41:48 -0400

    Exercise 34 Skeleton

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input method, this method will use a scanner to take in input.
+    -Create a string array full of 5 employ names, then create a temp array of size n-1.
+    -This array will be set after the name to remove has been decided
+    -Once the temp array has been fully set, display it.
+
+ */
+package baseline;
+
+public class Solution34 {
+    //Driver function, will run input().
+    public static void main(String[] args) {
+
+    }
+
+    //Creates an array of strings and removes a specified value from that array.
+    private static void input()
+    {
+        //This function uses a for loop to remove the chosen value
+
+    }
+}
