d425bb839f42d6d027408df421b3f8f7e7e145f6
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 18:27:33 -0400

    Exercise 33 skeleton added

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input function to handle all user input
+    -Create a string array the values should be:
+        -Yes
+        -No
+        -Maybe
+        -Ask again later
+    -Use a random number from 0 to 3 to determine which value to display
+ */
+package baseline;
+
+public class Solution33 {
+    //Driver method, runs input
+    public static void main(String[] args) {
+
+    }
+
+    //This function will take in a question, create a list of answers and then choose an answer using a random number.
+    private static void input()
+    {
+
+    }
+
+}
