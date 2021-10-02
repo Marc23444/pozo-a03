a71b7f101584d59b74af058789b5ed4212d6ccd2
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 19:01:16 -0400

    Finished Exercise33, no junit yet

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -14,15 +14,31 @@
  */
 package baseline;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution33 {
     //Driver method, runs input
     public static void main(String[] args) {
-
+        input();
     }
 
     //This function will take in a question, create a list of answers and then choose an answer using a random number.
     private static void input()
     {
+        //RNG = Random Number Generator
+        Scanner in = new Scanner(System.in);
+        int randNum;
+        String[] answers = {"Yes.","No.","Maybe.","Ask again later."};
+        Random RNG = new Random();
+
+        randNum = RNG.nextInt(4);
+
+        System.out.println("What's your question?");
+        in.nextLine();
+
+        System.out.println(answers[randNum]);
+
 
     }
 
