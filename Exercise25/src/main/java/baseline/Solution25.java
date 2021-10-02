49a86360f48d9a3ba96b81db9e7070ce6e844b1b
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-02 17:30:14 -0400

    Exercise 31 skeleton

--- Exercise25/src/main/java/baseline/Solution25.java
+++ Exercise25/src/main/java/baseline/Solution25.java
@@ -19,22 +19,98 @@
  */
 package baseline;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class Solution25 {
     //Calls input
     public static void main(String[] args) {
-
+        input();
     }
 
     //Takes in input and displays output
-    private static void input()
-    {
+    private static void input() {
+        //Creates a scanner
+        Scanner in = new Scanner(System.in);
+
+        String password;
+
+        System.out.println("Enter a password:");
+        password = in.nextLine();
+
+        switch (passwordValidator(password))
+        {
+        case 1:
+            System.out.println(password+" is a very weak password.");
+            break;
+        case 2:
+            System.out.println(password+" is a  weak password.");
+            break;
+        case 3:
+            System.out.println(password+" is a strong password.");
+            break;
+        case 4:
+            System.out.println(password+" is a very strong password.");
+            break;
+        case 5:
+            System.out.println(password+" is of unknown strength.");
+            break;
+        default:
+            break;
+        }
+
+
+
 
     }
 
-    //Takes in a string and checks how strong it is
+    //Takes in a string and checks how strong it is, this function is essentially a manager
+
     private static int passwordValidator(String password)
     {
+        //Fewer than 8 means the pass is weak
+        if(password.length() < 8)
+        {
+            if(! password.matches(".*[^a-z].*") || !password.matches(".*[^A-Z].*"))
+            {
+                //Weak
+                return 2;
+            }
+            else if (password.matches(".*\\d.*"))
+            {
+                //Very Weak;
+                return 1;
+            }
+            else
+            {
+                return 5;
+            }
 
+        }
+        //Greater means its at least strong
+        else
+        {
+            if(password.matches(".*\\d.*"))
+            {
+                if(password.matches(".*[^a-z].*") || password.matches(".*[^A-Z].*"))
+                {
+                    Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+                    Matcher m = p.matcher(password);
+                    boolean b = m.find();
+                    if(b)
+                    {
+                        //Very Strong password
+                        return 4;
+                    }
+                    //Strong password
+                    return 3;
+                }
+            }
+            return 5;
+        }
     }
 
 }
---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input method that will take in and validate user input
+    -Once input is validated the data is passed through an output function
+        -This function will have a while loop that displays the intensity and rate
+        -Heart rate will be calculated in a function called calcHR
+        -Finally once the HR is calculated it will be displayed in a table
+ */
+
+package baseline;
+
+public class Solution31 {
+
+    //Driver function, runs input
+    public static void main(String[] args) {
+
+    }
+
+    //Takes in input using a scanner, makes sure that input is valid
+    private static void input()
+    {
+
+    }
+
+    //Takes in age and heart rate once they've been validated, displays the output
+    private static void output(int age, int HR)
+    {
+        //Output will be displayed in a while loop, calcHR will be used to calculate the heart rate at each intensity.
+
+    }
+
+    //Used to calculate heart rate at each intensity
+    private static int calcHR(int age, int HR, int intensity)
+    {
+        //Uses the formula: TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+    }
+
+}
