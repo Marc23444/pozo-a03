1510142c200c6a6e21685dd6e8e76dc5e5139266
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 14:12:22 -0400

    Exercise35 Skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create a function called input to take in user input
+        -The user will enter names in a while loop until the give a blank answer
+        -The names will be placed in an arraylist
+    -A random number will then be generated using a function called generateWinner
+        -Generate winner will take in the lists size and generate a number from 0 to n
+    -The name related to this number will then be displayed by the input function
+
+ */
+package baseline;
+
+public class Solution35 {
+    //Driver function, will call input()
+    public static void main(String[] args) {
+
+    }
+
+    //Will take in user input, call generateWinner and display the winner
+    private static void input()
+    {
+        //A scanner, arraylist and while loop will be used
+
+    }
+
+    //Will use the Rand class to generate a random number for 0 to size
+    private static int generateWinner(int size)
+    {
+
+    }
+}
