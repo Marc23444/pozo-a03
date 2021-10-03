68705a12b47c4e45173e03f3ecc21b439ee93011
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 15:47:53 -0400

    Exercise37 skeleton

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Use a method called input to take in user input
+        -Prompt for minimum length
+        -Prompt for how many special characters
+        -Prompt for how many numbers
+    -Use a method called generatePass to generate and return the password string
+        -This method will use a for loop and the Rand class
+        -It will take in the all the data the user entered and will be called from inside input
+    -Finally display the password in input
+
+ */
+package baseline;
+
+public class Solution37 {
+    //Driver method, calls input
+    public static void main(String[] args) {
+
+    }
+
+    //Takes in user input with a scanner, calls generatePass
+    private static void input()
+    {
+
+    }
+
+    //Generates the password using the rand class
+    private static String generatePass(int length, int special,int numbers)
+    {
+
+    }
+
+}
