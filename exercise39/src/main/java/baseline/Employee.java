059d7b3a094888ab6eeb921007967012ea54bd9c
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 17:05:52 -0400

    Exercise39 Skeleton

---
+++ exercise39/src/main/java/baseline/Employee.java
@@ -0,0 +1,42 @@
+package baseline;
+
+public class Employee {
+
+    String firstName;
+    String lastName;
+    String position;
+    String seperationDate;
+
+
+    //Constructor sets the values of each employee
+    public Employee(String firstName, String lastName, String position, String seperationDate)
+    {
+
+
+    }
+
+    //THESE 4 FUNCTIONS RETURN FIELDS, THEY ARE GETTER METHODS
+
+    public String getFirstName()
+    {
+
+    }
+
+    public String getLastName()
+    {
+
+    }
+
+    public String getPosition()
+    {
+
+    }
+
+    public String getSeperationDate()
+    {
+
+    }
+
+
+
+}
---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -This program will ran from the main method and use helper methods throughout
+    -First there will be a class called employee which will have the fields:
+        -Firstname
+        -Lastname
+        -Position
+        -Seperation Date
+        -There will be getter methods for all of these fields as well as a constructor that sets them up
+    -Then the method createEmployees will create all 6 employee objects and put them in a map
+    -This map will then be sent to a function that will sort and print the employees by last name in a tabular format
+
+
+
+ */
+
+package baseline;
+
+import java.util.Map;
+
+public class Solution39 {
+    //Main method, will call createEmployees
+    public static void main(String[] args) {
+
+    }
+
+    //Will create all employees objects and place them in the map, data is hardcoded
+    private static Map<Integer,Employee> createEmployees()
+    {
+
+    }
+
+    //Will sort employees by looking at the first character of their last name, will then display the sorted map
+    private  static void sortEmployees(Map<Integer,Employee> employeeMap)
+    {
+
+    }
+
+}
