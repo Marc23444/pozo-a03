abd78c855e89ef8d0fdab481dba5061b979a43f8
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 16:49:34 -0400

    Finished exercise38, no Junit yet

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -16,11 +16,12 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution38 {
     //Driver function, calls input
     public static void main(String[] args) {
-
+        input();
     }
 
     //This function will use a scanner to take in the space separated input
@@ -28,12 +29,50 @@ public class Solution38 {
     private static void input()
     {
         //filterEvenNumbers will be called here
+        Scanner in = new Scanner(System.in);
+        String inputStr;
+        ArrayList<Integer> numList = new ArrayList<>();
+
+        System.out.println("Enter a list of numbers, separated by spaces:");
+        inputStr = in.nextLine();
+
+
+
+        for (int i = 0; i < inputStr.length(); i++)
+        {
+            if(inputStr.charAt(i) != ' ')
+            {
+
+                numList.add(Integer.parseInt(String.valueOf(inputStr.charAt(i))));
+
+            }
+        }
+
+        numList = filterEvenNumbers(numList);
+
+        System.out.print("The even numbers are");
+
+        //Prints out the filtered list
+        for (Integer integer : numList) {
+            System.out.print(" " + integer);
+        }
+
     }
 
     //Will use a for loop and a modulus operator to remove any odd numbers from the list
     private static ArrayList<Integer> filterEvenNumbers(ArrayList<Integer> numList)
     {
+        ArrayList<Integer> evenList = new ArrayList<>();
 
+        //Checks every element in the list and adds to evenList when an element is even
+        for (Integer integer : numList) {
+            if (integer % 2 == 0) {
+                evenList.add(integer);
+            }
+        }
+
+
+        return evenList;
     }
 
 }
