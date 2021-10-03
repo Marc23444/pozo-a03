c146a9ca2e61bd9c4a30e11a4d23a0a59a22671f
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 14:31:03 -0400

    Finished 35, no junit yet

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -14,22 +14,45 @@
  */
 package baseline;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution35 {
     //Driver function, will call input()
     public static void main(String[] args) {
-
+        input();
     }
 
     //Will take in user input, call generateWinner and display the winner
     private static void input()
     {
         //A scanner, arraylist and while loop will be used
+        Scanner in = new Scanner(System.in);
+        String inputStr = "default";
+        ArrayList<String> names = new ArrayList<String>();
+
+        //This loop runs until the user enters nothing
+        while(!inputStr.equals(""))
+        {
+            System.out.println("Enter a name:");
+            inputStr = in.nextLine();
+            if(!inputStr.equals(""))
+            {
+                names.add(inputStr);
+            }
+        }
+
+        //Output
+        System.out.println("The winner is... "+ names.get(generateWinner(names.size())));
 
     }
 
     //Will use the Rand class to generate a random number for 0 to size
     private static int generateWinner(int size)
     {
+        Random rand = new Random();
 
+        return rand.nextInt(size);
     }
 }
