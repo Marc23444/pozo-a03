8648033c0f8d5fb2a77912f719198726c38ff728
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 15:38:07 -0400

    Finished 36, no junit

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -16,47 +16,124 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution36 {
     //Main driver function, will call input
     public static void main(String[] args) {
-
+        input();
     }
 
     //Uses a while loop and scanner to take in input, stores input in an arrayList
     private static void input()
     {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Double> numList = new ArrayList<Double>();
+        String inputStr = "default";
+
+        while(!inputStr.equals("done"))
+        {
+            System.out.println("Enter a number:");
+            inputStr = in.nextLine();
+
+            try{
+                if(!inputStr.equals("done"))
+                {
+                    numList.add(Double.parseDouble(inputStr));
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Please enter a valid input");
+            }
+        }
+
+        output(numList);
+
 
     }
 
     //Takes in an arrayList from input, will call for and display average, max, min and std
     private static void output(ArrayList<Double> numList)
     {
+        System.out.print("Numbers: ");
+        for(int i = 0; i < numList.size();i++)
+        {
+            System.out.print(numList.get(i)+", ");
+        }
 
+        System.out.println("\nThe average is "+average(numList));
+
+        System.out.println("The minimum is "+min(numList));
+
+        System.out.println("The maximum is "+max(numList));
+
+        System.out.printf("The standard deviation is %.2f",std(numList));
     }
 
     //Calculates and returns the average value of the list
     private static double average(ArrayList<Double> numList)
     {
+        double temp = 0;
 
+        for(double num : numList)
+        {
+            temp+=num;
+        }
+
+        return (temp / numList.size());
     }
 
-    //Calculates and returns the max value in the list
-    private static double max(ArrayList<Double> numList)
-    {
-
-    }
 
     //Calculates and returns the min value in the list
     private static double min(ArrayList<Double> numList)
     {
+        double min = numList.get(0);
+        for(double num : numList)
+        {
+            if(num < min)
+            {
+                min = num;
+            }
+        }
 
+        return min;
+    }
+
+
+    //Calculates and returns the max value in the list
+    private static double max(ArrayList<Double> numList)
+    {
+        double max = 0;
+
+        for(double num : numList)
+        {
+            if(num > max)
+            {
+                max = num;
+            }
+        }
+
+        return max;
     }
 
     //Calculates and returns the standard deviation
     private static double std(ArrayList<Double> numList)
     {
+        double mean = average(numList);
+        double std = 0.0;
 
+        for(int i = 0; i < numList.size();i++)
+        {
+            std = std + Math.pow((numList.get(i)-mean),2);
+        }
+
+        std = std / numList.size();
+
+        std = Math.sqrt(std);
+
+
+        return std;
     }
 
 
