ba9fe45603f7f1a3461ee2c3fe977a3eb2467992
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 14:41:52 -0400

    Exercise36 Skeleton

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,63 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create a function called input that will use a scanner to take in numbers until the user enters done.
+        -These numbers will be placed in an array list
+        -A while loop will be used to take in input
+        -All bad input will be ignored using a try catch statement
+    -This list will be sent to a function called output
+    -Output will call 4 functions, average, max, min and std
+    -These 4 functions will have a double return type and the returned value will be displayed in output
+
+ */
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution36 {
+    //Main driver function, will call input
+    public static void main(String[] args) {
+
+    }
+
+    //Uses a while loop and scanner to take in input, stores input in an arrayList
+    private static void input()
+    {
+
+    }
+
+    //Takes in an arrayList from input, will call for and display average, max, min and std
+    private static void output(ArrayList<Double> numList)
+    {
+
+    }
+
+    //Calculates and returns the average value of the list
+    private static double average(ArrayList<Double> numList)
+    {
+
+    }
+
+    //Calculates and returns the max value in the list
+    private static double max(ArrayList<Double> numList)
+    {
+
+    }
+
+    //Calculates and returns the min value in the list
+    private static double min(ArrayList<Double> numList)
+    {
+
+    }
+
+    //Calculates and returns the standard deviation
+    private static double std(ArrayList<Double> numList)
+    {
+
+    }
+
+
+}
