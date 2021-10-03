90dcbb01c8d7d88eca28769808609516748873be
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-10-03 16:14:27 -0400

    Exercise38 Skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marcelino Pozo
+ */
+/*
+    PSUEDOCODE
+    -Create an input function that uses a scanner to handle input
+        -Take in input as a single space separated string
+        -Convert that string to an int arrayList
+    -Pass that arrayList through a function called filterEvenNumbers
+        -This function will use a for loop and modulus operator to determine the even and odd numbers
+        -This function will remove any odd numbers and return the filtered arrayList
+    -The filtered list is printed in input
+
+ */
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution38 {
+    //Driver function, calls input
+    public static void main(String[] args) {
+
+    }
+
+    //This function will use a scanner to take in the space separated input
+    //It will then convert that input into an ArrayList of integers
+    private static void input()
+    {
+        //filterEvenNumbers will be called here
+    }
+
+    //Will use a for loop and a modulus operator to remove any odd numbers from the list
+    private static ArrayList<Integer> filterEvenNumbers(ArrayList<Integer> numList)
+    {
+
+    }
+
+}
